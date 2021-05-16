bb8a8340a09f4d0a361f75d46f11011778ba08bb
NormanGraham <84263225+NormanGraham@users.noreply.github.com>
2021-05-16 04:04:37 -0400

    Update xy_tcpip.h

--- xy_tcpip.h
+++ xy_tcpip.h
@@ -5,24 +5,61 @@
    
     class xy_buffer { 
        public: 
-           char *ptr = NULL; 
+           char *ptrb = NULL; 
            unsigned short allocated = 0;
            unsigned short ip = 0;
-      
-           void bufferAllocate( unsigned short A ) { 
-                   if ( ptr )  
-                     delete [] ptr; 
-                   ptr = new unsigned char[ A ];
+           unsigned errorCount = 0; 
+       
+           void bShallowCopy( xy_buffer *xy ) { 
+              ptrb = xy->ptrb; 
+              allocated = xy->allocated; 
+              ip = xy->ip;
+              // does not copy errorCount.
+           }; 
+       
+           void Set( int offset, unsigned char *src, unsigned short len ) {
+              if ( offset < 0 ) 
+                 errorCount++; 
+              else {
+                   unsigned A = allocated; 
+                   unsigned char *tmpPtr = ptrb; 
+                   for (; (A) && (offset); offset--, A--, tmpPtr++ )
+                      ; 
+                   if ( !A ) { 
+                      errorCount++; 
+                   } 
+                   else { 
+                      memcpy( tmpPtr, src, len > A ? A : len );   
+                   }                     
+              }
+           }; 
+           void bAllocate( unsigned short A ) { 
+                   if ( ptrb )  
+                     delete [] ptrb;
+                   ptrb  = NULL;
                    ip = 0; 
-                   allocated = A;
+                   allocated = 0; 
+                   if ( A < (16*1024) ) {    // use the arbitrary 16k max.  
+                      ptrb = new unsigned char[ A + 1 ];
+                      memset(ptrb, 0, A+1); 
+                      allocated = A;
+                   }
+                   else 
+                      errorCount++; 
            }; 
            void bufferFree() { 
-                 if ( ptr ) {
-                   delete [] ptr ;
+                 if ( ptrb ) {
+                   delete [] ptrb ;
+                   ptrb = NULL; 
                  }
                  allocated = 0; 
                  ip = 0;
            };
+           void prefix( xy_buffer *xb) {
+              xy_buffer newxy;
+              newxy.bufferAllocate ( allocated + xb.allocated) ; 
+                    
+           }; 
            ~xy_buffer() { 
              bufferFree(); 
            }; 
